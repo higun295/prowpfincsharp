255c45ce74a05bd45639d59a9f147ec282231740
HEEKWON <higun295@gmail.com>
2022-02-28 10:32:47 +0900

    20220228_1

---
+++ ProWPFinCSharp/WpfApp2/Window1.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace WpfApp2 {
+    public class Window1 : Window {
+        private Button button1;
+
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            // Configure the form.
+            this.Width = this.Height = 285;
+            this.Left = this.Top = 100;
+            this.Title = "Code-Only Window";
+
+            // Create a container to hold a button.
+            DockPanel panel = new DockPanel();
+
+            // Create the button.
+            button1 = new Button();
+            button1.Content = "Please click me.";
+            button1.Margin = new Thickness(30);
+
+            // Attach the event handler.
+            button1.Click += Button1_Click;
+
+            // Place the button in the panel.
+            IAddChild container = panel;
+            container.AddChild(button1);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e) {
+            button1.Content = "Thank you.";
+        }
+    }
+}
