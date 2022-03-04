dbb80e6ab1a172633acbe2b34e408b634482edac
HEEKWON <higun295@gmail.com>
2022-03-04 09:10:33 +0900

    20220304_1

--- ProWPFinCSharp/Chapter3_Layout/MainWindow.xaml.cs
+++ ProWPFinCSharp/Chapter3_Layout/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Chapter3_Layout.Chapter3_Example;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Chapter3_Layout {
     /// <summary>
@@ -20,6 +8,21 @@ namespace Chapter3_Layout {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            WrapPanelWindow wrapPanelWindow = new WrapPanelWindow();
+            wrapPanelWindow.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+            DockPanelWindow dockPanelWindow = new DockPanelWindow();
+            dockPanelWindow.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) {
+            NestingLayoutContainersWindow nestingLayoutContainersWindow = new NestingLayoutContainersWindow();
+            nestingLayoutContainersWindow.ShowDialog();
         }
     }
 }
