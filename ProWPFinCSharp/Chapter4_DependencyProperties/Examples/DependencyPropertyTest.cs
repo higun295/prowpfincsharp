db6ca9bbe3676f0062b4c4a0e1eff8258914d040
HEEKWON <higun295@gmail.com>
2022-03-09 19:27:25 +0900

    20220309_2

--- ProWPFinCSharp/Chapter4_DependencyProperties/Examples/DependencyPropertyTest.cs
+++ ProWPFinCSharp/Chapter4_DependencyProperties/Examples/DependencyPropertyTest.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace Chapter4_DependencyProperties.Examples {
-    public class DependencyPropertyTest : FrameworkElement {
-        public static readonly DependencyProperty MarginProperty;
-
-        public DependencyPropertyTest() {
-            MarginProperty = DependencyProperty.Register("Margin", typeof(Thickness), new FrameworkElement(), new FrameworkPropertyMetadata(default(Thickness), FrameworkPropertyMetadataOptions.AffectsMeasure), null);
-        }
+    class DependencyPropertyTest {
     }
 }
