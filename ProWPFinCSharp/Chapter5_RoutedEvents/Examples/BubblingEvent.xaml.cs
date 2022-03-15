50c4512ece9b0da60bbbbc458a721d9b7bd93b05
HEEKWON <higun295@gmail.com>
2022-03-15 10:13:16 +0900

    20220315_1

---
+++ ProWPFinCSharp/Chapter5_RoutedEvents/Examples/BubblingEvent.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Chapter5_RoutedEvents.Examples {
+    /// <summary>
+    /// Interaction logic for BubblingEvent.xaml
+    /// </summary>
+    public partial class BubblingEvent : Window {
+        protected int eventCounter = 0;
+
+        public BubblingEvent() {
+            InitializeComponent();
+        }
+
+        private void SomethingClicked(object sender, MouseButtonEventArgs e) {
+            eventCounter++;
+            string message = "#" + eventCounter.ToString() + ":\r\n" +
+                " Sender : " + sender.ToString() + "\r\n" +
+                " Source : " + e.Source + "\r\n" +
+                " Original Source : " + e.OriginalSource;
+
+            lstMessages.Items.Add(message);
+            e.Handled = (bool)chkHandle.IsChecked;
+        }
+
+        private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e) {
+
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e) {
+            lstMessages.Items.Clear();
+            eventCounter = 0;
+        }
+    }
+}
